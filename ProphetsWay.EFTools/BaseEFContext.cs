519312b9160d5cf627437df098d6eb909c9f27bb
G. Gordon Nasseri <ProphetManX@gmail.com>
2020-05-23 17:47:17 -0400

    created a base DataAccess class for use, and a BaseEFContext class, so it ensures the context used by the BaseEFDataAccess class has a connection string constructor on it

---
+++ ProphetsWay.EFTools/BaseEFContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace ProphetsWay.EFTools
+{
+	public abstract class BaseEFContext  : DbContext
+	{
+		protected BaseEFContext(string connectionString) : base(connectionString) { }
+	}
+}
---
+++ ProphetsWay.EFTools/BaseEFDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using ProphetsWay.BaseDataAccess;
+using System;
+using System.Data.Entity;
+
+namespace ProphetsWay.EFTools
+{
+	public class BaseEFDataAccess<TContextType, TIdType> : BaseDataAccess<TIdType>, IBaseDataAccess<TIdType> where TContextType : BaseEFContext
+	{
+		protected DbContext Context { get; }
+
+		public BaseEFDataAccess(string connectionString)
+		{
+			Context = (DbContext)Activator.CreateInstance(typeof(TContextType), new object[] { connectionString });
+		}
+
+		public override void TransactionCommit()
+		{
+			Context.Database.CurrentTransaction.Commit();
+		}
+
+		public override void TransactionRollBack()
+		{
+			Context.Database.CurrentTransaction.Rollback();
+		}
+
+		public override void TransactionStart()
+		{
+			Context.Database.BeginTransaction();
+		}
+	}
+}
