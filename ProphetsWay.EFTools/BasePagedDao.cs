ff01a54d32b9ab8974c4933916e0794234872a30
G. Gordon Nasseri <ProphetManX@gmail.com>
2021-01-31 23:51:43 -0500

    ok, updated all the references to the newest versions of things, it all builds and tests ok

--- ProphetsWay.EFTools/BasePagedDao.cs
+++ ProphetsWay.EFTools/BasePagedDao.cs
@@ -1,7 +1,7 @@
-﻿#if NETSTANDARD2_0
+﻿#if NETSTANDARD2_0 || NETSTANDARD2_1 || NET5_0 || NETCOREAPP2_1 || NETCOREAPP3_1
 using Microsoft.EntityFrameworkCore;
 #endif
-#if NETSTANDARD2_1 || NET45 || NET461 || NET471 || NET472
+#if NET45 || NET451 || NET452|| NET46 || NET461 || NET471 || NET472 || NET48
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
 # endif
@@ -13,7 +13,7 @@ using System.Linq;
 namespace ProphetsWay.EFTools
 {
 	[Obsolete("You should be using one of the three new implementations:  BasePagedWithIntIdDao, BasePagedWithLongIdDao, or BasePagedWithGuidIdDao")]
-	public abstract class BasePagedDao<TEntityType, TIdType> : BaseDao<TEntityType, TIdType>, IBasePagedDao<TEntityType> where TEntityType : class, IBaseIdEntity<TIdType>
+	public abstract class BasePagedDao<TEntityType, TIdType> : BaseDao<TEntityType, TIdType>, IBasePagedDao<TEntityType> where TEntityType : class, IBaseIdEntity<TIdType> 
 	{
 		protected BasePagedDao(DbContext context) : base(context) { }
 
