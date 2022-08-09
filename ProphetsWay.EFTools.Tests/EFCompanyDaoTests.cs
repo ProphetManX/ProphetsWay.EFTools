7c69f3127371888b32546d8355520c0690068967
G. Gordon Nasseri <ProphetManX@gmail.com>
2022-08-09 01:33:43 -0400

    reverted test attributes, and created a flag in the app variables to account for not running unit tests within the pipeline

--- ProphetsWay.EFTools.Tests/EFCompanyDaoTests.cs
+++ ProphetsWay.EFTools.Tests/EFCompanyDaoTests.cs
@@ -1,11 +1,9 @@
 ﻿using ProphetsWay.Example.DataAccess.EF;
 using ProphetsWay.Example.DataAccess.IDaos;
 using ProphetsWay.Example.Tests;
-using Xunit;
 
 namespace ProphetsWay.EFTools.Tests
 {
-	[Collection("pipeline-skip")]
 	public class EFCompanyDaoTests : CompanyDaoTests
 	{
 		protected override ICompanyDao GetIExampleDataAccess => new ExampleDataAccess(Constants.ConnectionStrings.ProphetsWayExample);
--- ProphetsWay.EFTools.Tests/EFUserDaoTests.cs
+++ ProphetsWay.EFTools.Tests/EFUserDaoTests.cs
@@ -1,11 +1,9 @@
 ﻿using ProphetsWay.Example.DataAccess.EF;
 using ProphetsWay.Example.DataAccess.IDaos;
 using ProphetsWay.Example.Tests;
-using Xunit;
 
 namespace ProphetsWay.EFTools.Tests
 {
-	[Collection("pipeline-skip")]
 	public class EFUserDaoTests : UserDaoTests
 	{
 		protected override IUserDao GetIExampleDataAccess => new ExampleDataAccess(Constants.ConnectionStrings.ProphetsWayExample);
