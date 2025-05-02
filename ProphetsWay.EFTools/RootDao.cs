f95caa550766382d40fb2b428135c5a20c6f3e0e
ProphetManX <35438914+ProphetManX@users.noreply.github.com>
2025-05-01 23:44:44 -0400

    Add support for non-ID based entities in BaseSoftDao (#18)
    This update introduces `BaseNonIdDao<T>` and `BaseSoftNonIdDao<T>` classes to handle entities without traditional ID properties, accommodating composite keys and non-integer keys. The `RootDao` class now inherits from `RootNonIdDao<T>`, with several transaction management and entity operation methods updated or removed for improved code clarity. The `ProphetsWay.BaseDataAccess` package version has been upgraded from `2.4.0` to `2.5.0`, and the changelog has been updated accordingly.
    
    #15

--- ProphetsWay.EFTools/RootDao.cs
+++ ProphetsWay.EFTools/RootDao.cs
@@ -1,6 +1,5 @@
 ﻿#if NET8_0_OR_GREATER 
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 #endif
 #if NET461 || NET471 || NET48
 using System.Data.Entity;
@@ -12,37 +11,9 @@ using System.Linq;
 
 namespace ProphetsWay.EFTools
 {
-	internal class RootDao <T, TIdType> where T : class, IBaseIdEntity<TIdType> where TIdType : struct
+	internal class RootDao <T, TIdType> : RootNonIdDao<T> where T : class, IBaseIdEntity<TIdType> where TIdType : struct
 	{
-		public DbContext Context { get; }
-
-		public DbSet<T> Dataset { get; }
-
-#if NET8_0_OR_GREATER
-		private IDbContextTransaction _transaction;
-#endif
-#if NET461 || NET471 || NET48
-        private DbContextTransaction _transaction;
-#endif
-
-		internal RootDao(DbContext context)
-		{
-			Context = context;
-			Dataset = Context.Set<T>();
-			_transaction = null;
-		}
-
-		public int Delete(T item)
-		{
-			Dataset.Remove(item);
-			return Context.SaveChanges();
-		}
-
-		public void Insert(T item)
-		{
-			Dataset.Add(item);
-			Context.SaveChanges();
-		}
+		internal RootDao(DbContext context) : base(context) { }
 
 		public int Update(T item)
 		{
@@ -73,26 +44,6 @@ namespace ProphetsWay.EFTools
 		public IList<T> GetPaged(T item, int skip, int take)
 		{
 			return Dataset.OrderBy(x => x.Id).Skip(skip).Take(take).ToList();
-		}
-
-		public void EnsureBeginTransaction()
-		{
-			if (Context.Database.CurrentTransaction == null)
-			{
-				_transaction = Context.Database.BeginTransaction();
-			}
-		}
-
-		public void EnsureTransactionCommit()
-		{
-			_transaction?.Commit();
-			_transaction = null;
-		}
-
-		public void EnsureTransactionRollback()
-		{
-			_transaction?.Rollback();
-			_transaction = null;
 		}
 	}
 }
